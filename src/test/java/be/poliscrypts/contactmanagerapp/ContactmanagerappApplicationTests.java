324c57f71a09a440d2b7a4ed9a81ab2f8743757a
Mehdi BENBLIDIA <mehdibenblidia29@gmail.com>
2023-05-08 16:15:38 +0200

    Initialize contactmanagerapp

---
+++ src/test/java/be/poliscrypts/contactmanagerapp/ContactmanagerappApplicationTests.java
@@ -0,0 +1,13 @@
+package be.poliscrypts.contactmanagerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactmanagerappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
