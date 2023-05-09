d89fe71724d22329a7bf7664fc3dddc54e278b31
Mehdi BENBLIDIA <mehdibenblidia29@gmail.com>
2023-05-09 19:47:02 +0200

    creation of controller contactController and companycontroller

---
+++ src/main/java/be/poliscrypts/contactmanagerapp/controller/CompanyController.java
@@ -0,0 +1,57 @@
+package be.poliscrypts.contactmanagerapp.controller;
+
+import be.poliscrypts.contactmanagerapp.model.*;
+import be.poliscrypts.contactmanagerapp.service.*;
+import lombok.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/api/be/company")
+public class CompanyController {
+    private final CompanyServiceImpl companyService;
+
+    public CompanyController(CompanyServiceImpl companyService) {
+        this.companyService = companyService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Company>> getAllCompanies() {
+        List<Company> companiesList = companyService.findAllCompanies();
+        return new ResponseEntity<>(companiesList, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Company> getCompanyById(@PathVariable("id") Long id) {
+        Company company = companyService.findCompanyById(id);
+        return new ResponseEntity<>(company, HttpStatus.OK);
+    }
+
+    @GetMapping("/{uuid}")
+    public ResponseEntity<Company> getCompanyByUuid(@PathVariable("uuid") UUID uuid) {
+        Company company = companyService.findCompanyByUuid(uuid);
+        return new ResponseEntity<>(company, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Company> addCompany(@RequestBody Company company) {
+        Company newCompany = companyService.addCompany(company);
+        return new ResponseEntity<>(newCompany, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Company> updateCompany(@RequestBody Company company) {
+        Company companyUpdated = companyService.updateCompany(company);
+        return new ResponseEntity<>(companyUpdated, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{uuid}")
+    public ResponseEntity<?> deleteCompany(@PathVariable("uuid") UUID uuid) {
+        companyService.deleteByUUID(uuid);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/be/poliscrypts/contactmanagerapp/controller/ContactController.java
@@ -0,0 +1,57 @@
+package be.poliscrypts.contactmanagerapp.controller;
+
+import be.poliscrypts.contactmanagerapp.model.*;
+import be.poliscrypts.contactmanagerapp.service.*;
+import lombok.*;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("api/be/contact")
+public class ContactController {
+
+    private final ContactServiceImpl contactService;
+
+    public ContactController(ContactServiceImpl contactService) {
+        this.contactService = contactService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Contact>> getAllContacts() {
+        List<Contact> contactList = contactService.findAllContacts();
+        return new ResponseEntity<>(contactList, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Contact> getContactById(@PathVariable("id") Long id) {
+        Contact contact = contactService.findContactById(id);
+        return new ResponseEntity<>(contact, HttpStatus.OK);
+    }
+
+    @GetMapping("/{uuid}")
+    public ResponseEntity<Contact> getContactById(@PathVariable("uuid") UUID uuid) {
+        Contact contact = contactService.findContactByUuid(uuid);
+        return new ResponseEntity<>(contact, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Contact> addContact(@RequestBody Contact contact) {
+        Contact NewContact = contactService.addContact(contact);
+        return new ResponseEntity<>(contact, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Contact> updateContact(@RequestBody Contact contact) {
+        Contact contactUpdated = contactService.updateContact(contact);
+        return new ResponseEntity<>(contact, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{uuid}")
+    public ResponseEntity<?> deleteContact(@PathVariable("uuid") UUID uuid) {
+        contactService.deleteByUUID(uuid);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
+
