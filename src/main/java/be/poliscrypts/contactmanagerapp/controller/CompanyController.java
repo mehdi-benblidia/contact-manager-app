36b278ddb94a629393c9f548937eceb0c7a30d98
Mehdi BENBLIDIA <mehdibenblidia29@gmail.com>
2023-05-15 07:38:52 +0200

    Refactor controllers + Annotation swagger

--- src/main/java/be/poliscrypts/contactmanagerapp/controller/CompanyController.java
+++ src/main/java/be/poliscrypts/contactmanagerapp/controller/CompanyController.java
@@ -2,25 +2,33 @@ package be.poliscrypts.contactmanagerapp.controller;
 
 import be.poliscrypts.contactmanagerapp.model.*;
 import be.poliscrypts.contactmanagerapp.service.*;
+import io.swagger.v3.oas.annotations.*;
+import lombok.extern.apachecommons.*;
 import org.springframework.http.*;
+//import org.springframework.security.access.prepost.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
 @RestController
 @RequestMapping("/api/be/company")
+@CommonsLog
 public class CompanyController {
     private final CompanyService companyService;
+    private final ContactService contactService;
 
-    public CompanyController(CompanyService companyService) {
+    public CompanyController(CompanyService companyService, ContactService contactService) {
         this.companyService = companyService;
+        this.contactService = contactService;
     }
 
     @GetMapping("/all")
+    @Operation(summary = "Get all companies", description = "Find all companies in the application")
     public ResponseEntity<List<Company>> getAllCompanies() {
         List<Company> companiesList = companyService.findAllCompanies();
         return new ResponseEntity<>(companiesList, HttpStatus.OK);
     }
+
 
     @GetMapping("/id/{id}")
     public ResponseEntity<Company> getCompanyById(@PathVariable("id") Long id) {
@@ -35,21 +43,41 @@ public class CompanyController {
     }
 
     @PostMapping("/add")
+    @Operation(summary = "Add anew company")
     public ResponseEntity<Company> addCompany(@RequestBody Company company) {
         Company newCompany = companyService.addCompany(company);
         return new ResponseEntity<>(newCompany, HttpStatus.CREATED);
     }
 
     @PutMapping("/update")
+    @Operation(summary = "Update an company")
     public ResponseEntity<Company> updateCompany(@RequestBody Company company) {
         Company companyUpdated = companyService.updateCompany(company);
         return new ResponseEntity<>(companyUpdated, HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{uuid}")
-    public ResponseEntity<?> deleteCompany(@PathVariable("uuid") UUID uuid) {
-        companyService.deleteByUUID(uuid);
+    @Operation(summary = "Delete a company by UUID")
+    public ResponseEntity<Void> deleteCompany(@PathVariable UUID uuid) {
+        try {
+            companyService.deleteCompanyByUuid(uuid);
+            return ResponseEntity.noContent().build();
+        } catch (NoSuchElementException ex) {
+            return ResponseEntity.notFound().build();
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PostMapping("/{companyUuid}/contact")
+    @Operation(summary = "Add a contact to a company")
+    public ResponseEntity<Void> addContactToCompany(@PathVariable("companyUuid") UUID companyUuid,
+                                                    @RequestBody Contact contactReqst) {
+        Company company = companyService.findCompanyByUuid(companyUuid);
+        Contact contact = contactService.findContactByUuid(contactReqst.getUuid());
+        companyService.addContactToCompany(company, contact.getId());
         return new ResponseEntity<>(HttpStatus.OK);
+
     }
 
 }
--- src/main/java/be/poliscrypts/contactmanagerapp/controller/ContactController.java
+++ src/main/java/be/poliscrypts/contactmanagerapp/controller/ContactController.java
@@ -2,22 +2,30 @@ package be.poliscrypts.contactmanagerapp.controller;
 
 import be.poliscrypts.contactmanagerapp.model.*;
 import be.poliscrypts.contactmanagerapp.service.*;
+import be.poliscrypts.contactmanagerapp.validator.*;
+import io.swagger.v3.oas.annotations.*;
+import lombok.extern.apachecommons.*;
 import org.springframework.http.*;
+//import org.springframework.security.access.prepost.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
 @RestController
 @RequestMapping("api/be/contact")
+@CommonsLog
 public class ContactController {
 
     private final ContactService contactService;
+    private final ContactValidator contactValidator;
 
-    public ContactController(ContactService contactService) {
+    public ContactController(ContactService contactService, ContactValidator contactValidator) {
         this.contactService = contactService;
+        this.contactValidator = contactValidator;
     }
 
     @GetMapping("/all")
+    @Operation(summary = "Get all contacts", description = "Find all contacts")
     public ResponseEntity<List<Contact>> getAllContacts() {
         List<Contact> contactList = contactService.findAllContacts();
         return new ResponseEntity<>(contactList, HttpStatus.OK);
@@ -29,28 +37,41 @@ public class ContactController {
         return new ResponseEntity<>(contact, HttpStatus.OK);
     }
 
+
     @GetMapping("/{uuid}")
     public ResponseEntity<Contact> getContactById(@PathVariable("uuid") UUID uuid) {
         Contact contact = contactService.findContactByUuid(uuid);
         return new ResponseEntity<>(contact, HttpStatus.OK);
     }
 
+
     @PostMapping("/add")
+    @Operation(summary = "Add a new contact")
     public ResponseEntity<Contact> addContact(@RequestBody Contact contact) {
-        Contact NewContact = contactService.addContact(contact);
-        return new ResponseEntity<>(contact, HttpStatus.CREATED);
+        contactValidator.validate(contact);
+        Contact newContact = contactService.addContact(contact);
+        return new ResponseEntity<>(newContact, HttpStatus.CREATED);
     }
 
     @PutMapping("/update")
+    @Operation(summary = "Update an contact")
     public ResponseEntity<Contact> updateContact(@RequestBody Contact contact) {
+        contactValidator.validate(contact);
         Contact contactUpdated = contactService.updateContact(contact);
-        return new ResponseEntity<>(contact, HttpStatus.OK);
+        return new ResponseEntity<>(contactUpdated, HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{uuid}")
-    public ResponseEntity<?> deleteContact(@PathVariable("uuid") UUID uuid) {
-        contactService.deleteByUUID(uuid);
-        return new ResponseEntity<>(HttpStatus.OK);
+    @Operation(summary = "Delete a contact by UUID")
+    public ResponseEntity<HttpStatus> deleteByUuid(@PathVariable UUID uuid) {
+        try {
+            contactService.deleteContactByUuid(uuid);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (NoSuchElementException ex) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
 
